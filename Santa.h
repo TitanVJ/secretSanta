1537d56a31e4e281bb137343c74a88ac429083b6
VJ <vijenderbakhshi@gmail.com>
2018-11-11 11:35:55 -0800

    added ifndef and put public var's and functions before the private ones

--- Santa.h
+++ Santa.h
@@ -1,7 +1,22 @@
+#ifndef SANTA_H
+#define SANTA_H
+
 #include <string>
 #include <vector>
 
 class Santa{
+    public:
+        // getters setters, others,
+        // might but friend class main so wont need the setters and getters 
+        
+        // Constructors
+        Santa();
+        Santa(std::string, std::string, int, int, int, std::vector<std::string>&);
+
+        // Utility
+        ~Santa();
+        // get file data? 
+    
     private:
         std::string name;
         std::string email;
@@ -14,16 +29,6 @@ class Santa{
         // questions.txt will be the questions asked during the 
         std::vector<std::string> details; // either this or mutiple strings with things 
 
-    public:
-        // getters setters, others,
-        // might but friend class main so wont need the setters and getters 
-        
-        // Constructors
-        Santa();
-        Santa(std::string, std::string, int, int, int, std::vector<std::string>&);
-
-        // Utility
-        ~Santa();
-        // get file data? 
-
 };
+
+#endif
