24a833adbeff69a4d05f875889a4118c93b0aa37
VJ <vijenderbakhshi@gmail.com>
2018-11-12 17:09:48 -0800

    removed destructor header cuz its not need. Also added << overload header

--- Santa.h
+++ Santa.h
@@ -6,16 +6,9 @@
 
 class Santa{
     public:
-        // getters setters, others,
-        // might but friend class main so wont need the setters and getters 
-        
         // Constructors
         Santa();
         Santa(std::string, std::string, int, int, int, std::vector<std::string>&);
-
-        // Utility
-        ~Santa();
-        // get file data? 
     
     private:
         std::string name;
@@ -28,6 +21,7 @@ class Santa{
         // details; everything after the email will be details. program usage will also be data.txt questions.txt
         // questions.txt will be the questions asked during the 
         std::vector<std::string> details; // either this or mutiple strings with things 
+        friend std::ostream& operator<<(std::ostream& output, const Santa&); 
 
 };
 
