ea38e24e0eee387eae37923ba10632a3e6b2d854
VJ <vijenderbakhshi@gmail.com>
2018-11-12 17:29:24 -0800

    added implementation of Santa.h

---
+++ Santa.cpp
@@ -0,0 +1,26 @@
+#include "Santa.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+Santa::Santa() : name(NULL), email(NULL), age(NULL), sfuId(NULL), details(NULL){}
+
+Santa::Santa(string name, string email, int age, int sfuId, int santaId, vector<string>& details) : name(name), email(email), age(age), sfuId(sfuId), santaId(santaId), details(details){}
+
+ostream& operator<<(ostream& out, const Santa& sObj){
+    // Go through all the data and concatenate into a
+    // nice looking string and output
+    // include sfu id since the program will only be run by execs
+
+    out << "Name:\t" << sObj.name << endl;
+    out << "Email\t" << sObj.email << endl;
+    out << "Age:\t" << sObj.age << endl;
+    out << "SFU Id:\t" << sObj.sfuId << endl;
+    out << "Santa Id:\t" << sObj.santaId << endl;
+    out << "Gifter Id:\t" << sObj.gifterId << endl;
+    out << "Details:\tLook at file[" << sObj.santaId << "]. in admin/" << endl;
+
+    return out;
+}
