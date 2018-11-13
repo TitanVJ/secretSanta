30b42bf2470fe825b715c08126bdd1ff5140c9aa
VJ <vijenderbakhshi@gmail.com>
2018-11-12 17:11:51 -0800

    << overloading operator was put under private by accident, moved it to public.

--- Santa.h
+++ Santa.h
@@ -9,7 +9,10 @@ class Santa{
         // Constructors
         Santa();
         Santa(std::string, std::string, int, int, int, std::vector<std::string>&);
-    
+
+        //Utility
+        friend std::ostream& operator<<(std::ostream& output, const Santa&); 
+
     private:
         std::string name;
         std::string email;
@@ -21,7 +24,6 @@ class Santa{
         // details; everything after the email will be details. program usage will also be data.txt questions.txt
         // questions.txt will be the questions asked during the 
         std::vector<std::string> details; // either this or mutiple strings with things 
-        friend std::ostream& operator<<(std::ostream& output, const Santa&); 
 
 };
 
