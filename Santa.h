72afcd4847fc76573e84c46175d8e105e55d4f5c
VJ <vijenderbakhshi@gmail.com>
2018-11-13 15:28:46 -0800

    added friend functions

--- Santa.h
+++ Santa.h
@@ -12,7 +12,9 @@ class Santa{
 
         //Utility
         friend std::ostream& operator<<(std::ostream& output, const Santa&); 
-
+        friend void shuffleAndRandomize(std::vector<Santa>& s);
+        friend void genSanta(std::vector<std::vector<std::string>>& data, std::vector<Santa>& santas);
+        
     private:
         std::string name;
         std::string email;
