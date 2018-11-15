aa08515328289ee9c11e39d8616f611193ccce61
VJ <vijenderbakhshi@gmail.com>
2018-11-14 18:21:26 -0800

    added code for user input to take in info about the outgoing email

--- santaEmailer.cpp
+++ santaEmailer.cpp
@@ -63,11 +63,49 @@ int main(int argc, char* argv[]){
     inFile.close();
     
     // Get mailio information
-    string smtpServer, smtpEmail, smtpPass, fromEmail, fromName, msgFile, subject;
+    string smtpServer, smtpEmail, smtpPass, fromEmail, fromName, msgFile, subject, line;
+    string msgContent = "";
     int port;
     // santaId, gifterId, SFUid, Name, Email
     //    0   ,    1    ,   2  ,   3 ,   4
 
+    // Get email information
+    cout << "This Test E-mail will contain info about the Secret Santa giftee." << endl;
+    cout << "Enter filename for the file that contains the email's message (include path if not in current dir): ";
+    cin >> msgFile;
+    cout << endl;
+
+    cout << "Note: Only the gmail SMTP Server will make a record of the email sent in your sent folder" << endl;
+    cout << "You can you any SMTP Server as long as you have the credentials to login, and then set the from field to whatever you want" << endl;
+    cout << "Consider the above note for the following prompts" << endl;
+    cout << endl;
+
+    cout << "Enter the SMTP Server: ";
+    cin >> smtpServer;
+    cout << "Enter the port number for the SMTP Server: "; 
+    cin >> port;
+    cout << endl;
+
+    cout << "Enter your login email for the given SMTP Server: ";
+    cin >> smtpEmail;
+    cout << "Enter your login password for the given SMTP Server: "; 
+    cin >> smtpPass;
+    cout << endl;
+
+    cout << "Enter the email to go in the \"FROM\" field of the email (this'll be the emails replies are sent to): "; 
+    cin >> fromEmail;
+    cout << "Enter name to appear in \"FROM\" field: ";
+    cin >> fromName;
+    cout << "Enter the email subject line: ";
+    cin >> subject;
+
+    ifstream emailMsg;
+    emailMsg.open(msgFile);
+
+    while(getline(emailMsg, line)){
+        msgContent += line + "\r\n";
+    }
+    emailMsg.close();
 
     // below nested loop is for testing
     for(auto i = santas.begin(); i != santas.end(); ++i){
